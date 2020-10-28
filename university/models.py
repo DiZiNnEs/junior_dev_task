8815794070b36cfd3f877a4adc624a6c0232da63
Iglek Kuat <dizinnes228@gmail.com>
2020-10-28 13:58:22 +0600

    refactor: add __str__ method

--- university/models.py
+++ university/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class University(models.Model):
     university_name = models.CharField(max_length=32)
     university_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.university_name + ' ' + self.university_description
