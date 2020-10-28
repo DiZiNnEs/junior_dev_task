7832e99c72c97fee12ff89792072a7c18046f5db
Iglek Kuat <dizinnes228@gmail.com>
2020-10-28 14:15:55 +0600

    feat: add form handle and add dates from form to databases

--- university/views.py
+++ university/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+
 from .models import University
+from .forms import UniversityForm
 
 
 def main(request):
+    form = UniversityForm()
+
+    if request.method == 'POST':
+        print(request.POST)
+        form = UniversityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
     database = University.objects.all()
-    context = {'university': database}
+    context = {'university': database,
+               'form': form}
     return render(request, 'university/index.html', context)
