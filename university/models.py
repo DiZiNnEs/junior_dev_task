06d636d04b08098e97cf66eb464b61e0e43e08ef
Iglek Kuat <dizinnes228@gmail.com>
2020-10-29 02:02:09 +0600

    feat: add new fields in model and make migrations

--- university/models.py
+++ university/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class University(models.Model):
     university_name = models.CharField(max_length=32)
     university_description = models.CharField(max_length=200)
+    address = models.CharField(max_length=50, blank=True)
+    items = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.university_name + ' ' + self.university_description
