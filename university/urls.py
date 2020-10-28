75d391094f29d322edb1ae1f0ba2e0c4578274dd
Iglek Kuat <dizinnes228@gmail.com>
2020-10-28 22:29:15 +0600

    feat: add link for logout

--- university/urls.py
+++ university/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('university/<str:pk>', views.university_page_delete, name='university_page'),
     path('register', views.register_page, name='register_page'),
     path('sign-in', views.login_page, name='authorization_page'),
+    path('logout', views.logout_user, name='logout'),
 ]
