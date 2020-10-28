ac8d87b366c544e25b011298ea7a8a42a35b6223
Iglek Kuat <dizinnes228@gmail.com>
2020-10-28 13:29:51 +0600

    feat: make standard logic for output databases to templates

--- university/views.py
+++ university/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import University
 
 
 def main(request):
-    return render(request, 'university/index.html')
+    database = University.objects.all()
+    context = {'university': database}
+    return render(request, 'university/index.html', context)
