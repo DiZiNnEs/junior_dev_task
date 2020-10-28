12a57e5a66c5aa815c15c510ecb36db48480bd57
Iglek Kuat <dizinnes228@gmail.com>
2020-10-29 02:33:41 +0600

    refactor: update models.py #2

--- university/models.py
+++ university/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class University(models.Model):
     university_name = models.CharField(max_length=32)
     address = models.CharField(max_length=50)
-    university_description = models.CharField(max_length=200)
-    items = models.CharField(max_length=100, default='Not filled by authors')
+    university_description = models.CharField(max_length=200, blank=True)
+    items = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.university_name + ' ' + self.university_description
